4efba0c767d44f49b327f357c63b7bd06eb38a31
yz_glb <goodMan_glb@atguigu.com>
2020-08-13 20:45:44 +0800

    aa

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,26 +1,24 @@
 <template>
   <div id="home">
     <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
-    <Swiper>
-      <SwiperItem v-for="item in banners">
-        <a :href="item.link"></a>
-      </SwiperItem>
-    </Swiper>
+    <HomeSwiper :banners = 'banners'></HomeSwiper>
+    <RecommendView :recommends = 'recommends'></RecommendView>
   </div>
 </template>
 
 <script>
   
   import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
   import {getHomeMultidata} from 'network/home'
-  import {Swiper, SwiperItem} from 'components/common/swiper'
 
   export default {
     name: 'Home',
     components: {
       NavBar,
-      Swiper,
-      SwiperItem
+      HomeSwiper,
+      RecommendView
     },
     data() {
       return {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'RecommendView',
+    props: {
+      recommends: {
+        type: Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .recommend {
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+    padding: 10px 0 20px;
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 85%;
+    margin-bottom: 10px;
+  }
+</style>
