b95e4cabe1d03431c6272e5e02c187c3008bd164
yz_glb <goodMan_glb@atguigu.com>
2020-08-13 19:01:40 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
+    <MainTabBar></MainTabBar>
   </div>
 </template>
 
 <script>
+  import MainTabBar from 'components/content/maintabbar/MainTabBar'
 
-
-export default {
-  name: 'App',
-  components: {
-
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    }
   }
-}
 </script>
 
 <style>
-
+  @import 'assets/css/base.css';
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="home">
+    <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+    <Swiper>
+      <SwiperItem v-for="item in banners">
+        <a :href="item.link"></a>
+      </SwiperItem>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+  
+  import NavBar from 'components/common/navbar/NavBar'
+  import {getHomeMultidata} from 'network/home'
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: 'Home',
+    components: {
+      NavBar,
+      Swiper,
+      SwiperItem
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    },
+  }
+</script>
+
+<style>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+</style>
