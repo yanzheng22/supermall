a994ca929dc5aa299b8cce21deed08b03b26e511
yz_glb <goodMan_glb@atguigu.com>
2020-09-11 16:25:48 +0800

    补充

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,13 +1,103 @@
 <template>
-  <h2>我的</h2>
+  <div id="profile">
+    <NavBar class="nav-bar"><div slot="center">购物商城</div></NavBar>
+
+    <UserInfo></UserInfo>
+
+    <section class="account">
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0.00</span>元
+        </div>
+        <div class="account-info">我的余额</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>个
+        </div>
+        <div class="account-info">我的优惠</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>分
+        </div>
+        <div class="account-info">我的积分</div>
+      </div>
+    </section>
+
+    <ListView :list-data="orderList" class="order-list"></ListView>
+    <ListView :list-data="serviceList" class="service-list"></ListView>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Profile'
-}
+  import UserInfo from './childComps/UserInfo'
+  import ListView from './childComps/ListView'
+  import NavBar from 'components/common/navbar/NavBar'
+
+	export default {
+		name: "Profile",
+    components: {
+		  UserInfo, ListView, NavBar
+    },
+    data: function () {
+		  return {
+		    orderList: [
+          {info: '我的消息'},
+          {info: '积分商城'},
+          {info: '会员卡'}
+        ],
+        serviceList: [
+          {info: '我的购物车'},
+          {info: '下载购物APP'}
+        ]
+      }
+    },
+
+	}
 </script>
 
-<style>
+<style scoped>
+  #profile {
+    background-color: #f2f2f2;
+  }
+
+  .nav-bar {
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
+
+  .account {
+    display: flex;
+  }
+
+  .account-item {
+    width: 100%;
+    background-color: #fff;
+    margin-right: 1px;
+    text-align: center;
+    color: #666;
+    font-size: 13px;
+    padding: 18px;
+  }
+
+  .account-item:last-of-type {
+    margin-right: 0;
+  }
+
+  .account-item .balance {
+    font-size: 24px;
+    font-weight: 700;
+    color: #ff5f3e;
+  }
+
+  .account-info {
+    margin-top: 6px;
+  }
+
+  .order-list, .service-list {
+    margin-top: 12px;
+  }
 
 </style>
